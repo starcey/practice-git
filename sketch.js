b4b6117dfb2580b58a25fcc7c9b6d79f638418a2
stacey <b110562045@gmail.com>
2019-06-16 16:33:04 +0800

    second commit

---
+++ sketch.js
@@ -0,0 +1,19 @@
+// Take a look at the HTML file where some things have been
+// added for mobile viewing
+
+function setup() {
+  // Make the canvas the size of the mobile device screen
+  createCanvas(windowWidth, windowHeight);
+  background(200);
+}
+
+function touchMoved() {
+  strokeWeight(10)
+  stroke(0);
+  // Here touchX and touchY act like mouseX and mouseY
+  line(touchX, touchY, ptouchX, ptouchY);
+  // line(mouseX, mouseY, pmouseX, pmouseY);
+
+  // This prevents dragging screen around
+  return false;
+}
